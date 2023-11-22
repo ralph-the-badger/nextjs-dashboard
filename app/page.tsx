834202335b1fa067d925dfae3b3c5b4df576f38c
ralph_the_badger <badger_rr@gmx.de>
2023-11-22 18:27:24 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
-import { lusitana } from './ui/fonts';
+// import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
@@ -15,7 +15,8 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape} />
           <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+            className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}
+            // className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
